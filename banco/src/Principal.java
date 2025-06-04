faf32085c5b5ba0596b10aea21307a074d337dc2
Leonardo Augusto da Silva Pascoal <leonardo.pascoal@saocarlos.local>
2025-06-04 09:53:41 -0300

    att 04/06

--- banco/src/Principal.java
+++ banco/src/Principal.java
@@ -1,3 +1,12 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Principal {
 
+    public static void main(String[] args) {
+        DayOfWeek diaDaSemana = LocalDate.now().getDayOfWeek();
+        System.out.println(diaDaSemana);
+    }
+    
+    
 }
---
+++ banco/src/com/algaworks/date_ZoneDateTime/Principal.java
@@ -0,0 +1,12 @@
+package com.algaworks.date_ZoneDateTime;
+
+import java.time.ZonedDateTime;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        ZonedDateTime dataHoraFusoPadrao = ZonedDateTime.now();
+
+        System.out.println(dataHoraFusoPadrao);
+    }
+}
